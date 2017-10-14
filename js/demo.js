8efda5e0e8300ff1fa484d35009c4748fa6878b1
Omar Alvarez Alcantara <omaralvarez01.oaa@gmail.com>
2017-10-14 01:12:22 -0500

    sweet

--- js/demo.js
+++ js/demo.js
@@ -182,7 +182,7 @@ function processCompress(input) {
 
 
 										   swal({
-												  title: '<i style="font-style: normal;">Datos obtenidos</i>',											  
+												  text: '<i style="font-style: normal;">Datos obtenidos</i>',
 												  html:
 													"<table cellpadding='0' cellspacing='0' border=0 style='margin-top:19px;border-color:transparent; font-size:11px; width: 100%;border-collapse: separate;border-spacing:  7px;'><tr><td align='left' valign='top' style='padding-right:2em;'>Nombre</td><td align='left' style='font-weight:bold;max-width: 250px;'>"+ data.Nombre+"</td><td></td></tr><tr><td align='left' valign='top' style='padding-right:2em;'>Paterno</td><td align='left' style='font-weight:bold;max-width: 250px;'>"+data.Paterno+"</td><td></td></tr><tr><td align='left' valign='top' style='padding-right:2em;'>Materno</td><td align='left' style='font-weight:bold;max-width: 250px;'>"+data.Materno+"</td><td></td></tr><tr><td align='left' valign='top' style='padding-right:2em;'>Calle</td><td align='left' style='font-weight:bold;max-width: 250px;'>"+data.calle+"</td><td></td></tr><tr><td align='left' valign='top' style='padding-right:2em;'>CP</td><td align='left' style='font-weight:bold;max-width: 250px;'>"+data.codigoPostal+"</td><td></td></tr><tr><td align='left' valign='top' style='padding-right:2em;'>Colonia</td><td align='left' style='font-weight:bold;max-width: 250px;'>"+data.colonia+"</td><td></td></tr><tr><td align='left' valign='top' style='padding-right:2em;'>Fecha nac</td><td align='left' style='font-weight:bold;max-width: 250px;'>"+data.fechaDeNacimiento+"</td><td></td></tr></table>",
 												  showCloseButton: true,
